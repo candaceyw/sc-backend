39a62c44bbe240d7c5b3669b1d798b254bfe95bd
candacewilson <candaceyw@gmail.com>
2020-10-17 15:14:25 -0400

    save

--- knexfile.js
+++ knexfile.js
@@ -19,21 +19,21 @@
 require('dotenv').config('./.env');
 
 module.exports = {
-	development: {
-		client: 'pg',
-		connection: process.env.DB_URL,
-		migrations: {
-			directory: './database/migrations',
-			tableName: 'knex_migrations',
-		},
-		seeds: { directory: './database/seeds' },
-	},
+	// development: {
+	// 	client: 'pg',
+	// 	connection: process.env.DB_URL,
+	// 	migrations: {
+	// 		directory: './database/migrations',
+	// 		tableName: 'knex_migrations',
+	// 	},
+	// 	seeds: { directory: './database/seeds' },
+	// },
 	production: {
 		client: 'pg',
 		connection: process.env.DATABASE_URL,
 		migrations: {
 			directory: './database/migrations',
-			// tableName: 'registration',
+			tableName: 'registration',
 		},
 		seeds: { directory: './database/seeds' },
 	},
