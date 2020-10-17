e0ba5081f96e5f59adde46b03448dec9fcfbc6a6
candacewilson <candaceyw@gmail.com>
2020-10-17 15:27:48 -0400

    knex file changes

--- knexfile.js
+++ knexfile.js
@@ -16,35 +16,28 @@
 // 		},
 // 	},
 // };
-require('dotenv').config('./.env');
+
+require('dotenv').config();
 
 module.exports = {
-	// development: {
-	// 	client: 'pg',
-	// 	connection: process.env.DB_URL,
-	// 	migrations: {
-	// 		directory: './database/migrations',
-	// 		tableName: 'knex_migrations',
-	// 	},
-	// 	seeds: { directory: './database/seeds' },
-	// },
+	development: {
+		client: 'pg',
+		connection: process.env.DB_URL,
+		migrations: {
+			directory: './database/migrations',
+			tableName: 'knex_migrations',
+		},
+		seeds: { directory: './database/seeds' },
+	},
 	production: {
 		client: 'pg',
 		connection: process.env.DATABASE_URL,
 		migrations: {
 			directory: './database/migrations',
+			tableName: 'knex_migrations',
 		},
 		seeds: {
 			directory: './database/seeds',
 		},
 	},
-	// test: {
-	// 	client: 'pg',
-	// 	connection: process.env.DATABASE_URL,
-	// 	migrations: {
-	// 		directory: './database/migrations',
-	// 		tableName: 'registration',
-	// 	},
-	// 	seeds: { directory: './database/seeds' },
-	// },
 };
